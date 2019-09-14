b9ac6a5bfdc6d680790606d814092c57ceba9aed
marcopandolfo <marcopandolfo577@gmail.com>
2019-09-14 12:28:25 -0300

    Question Model

---
+++ MobileQuiz/Models/QuestionModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileQuiz.Models
+{
+    public class QuestionModel
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Category { get; set; }
+        public string IncorrectAnswers { get; set; }
+        public QuestionModel(int id, string question, string correctAnswer, string category, string incorrectAnswers)
+        {
+            Id = id;
+            Question = question;
+            CorrectAnswer = correctAnswer;
+            Category = category;
+            IncorrectAnswers = incorrectAnswers;
+        }
+    }
+}

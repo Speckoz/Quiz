aca121a291a13ecd0bff0cb1bce8df4af0b2161b
marcopandolfo <marcopandolfo577@gmail.com>
2019-09-20 22:31:19 -0300

    Typo

--- MobileQuiz/Services/UserService.cs
+++ MobileQuiz/Services/UserService.cs
@@ -1,21 +1,18 @@
 ﻿using MobileQuiz.Models;
 using MobileQuiz.Services.Helpers;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MobileQuiz.Services
 {
     public static class UserService
     {
-        public static List<UserModel> users { get; set; } = Seed.SeedUsers();
+        public static List<UserModel> Users { get; set; } = Seed.SeedUsers();
 
-        public static void SaveUser(UserModel user) => users.Add(user);
+        public static void SaveUser(UserModel user) => Users.Add(user);
 
         public static UserModel SearchUser(string login, string password)
         {
-            return users.Find(u => u.Login == login && u.Password == password);
+            return Users.Find(u => u.Login == login && u.Password == password);
         }
     }
 }

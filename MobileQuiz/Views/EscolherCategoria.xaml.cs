2bf3a648c974ca4247b7eed8994366a7b78283eb
marcopandolfo <marcopandolfo577@gmail.com>
2019-09-20 22:49:25 -0300

    Melhorando handlers

--- MobileQuiz/Views/EscolherCategoria.xaml.cs
+++ MobileQuiz/Views/EscolherCategoria.xaml.cs
@@ -1,13 +1,9 @@
 ﻿using MobileQuiz.Models;
 using MobileQuiz.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static MobileQuiz.Services.QuestionService;
 
 namespace MobileQuiz.Views
 {
@@ -19,26 +15,11 @@ namespace MobileQuiz.Views
             InitializeComponent();
         }
 
-        private void Todas_Clicked(object sender, EventArgs e)
+        private void Categoria_Clicked(object sender, EventArgs e)
         {
-            QuestionService.GetRandomQuestion();
-            Application.Current.MainPage = new Jogo();
-        }
-        private void Ciencia_Clicked(object sender, EventArgs e)
-        {
-            QuestionService.GetRandomQuestion("Ciencia");
-        }
-        private void Arte_Clicked(object sender, EventArgs e)
-        {
-            QuestionService.GetRandomQuestion("Arte");
-        }
-        private void Historia_Clicked(object sender, EventArgs e)
-        {
-            QuestionService.GetRandomQuestion("Historia");
-        }
-        private void Geografia_Clicked(object sender, EventArgs e)
-        {
-            QuestionService.GetRandomQuestion("Geografia");
+            Button btn = sender as Button;
+            QuestionModel question = btn.Text == "Todas" ? GetRandomQuestion() : GetRandomQuestion(btn.Text);
+            //Application.Current.MainPage = new Jogo(question);
         }
     }
 }
--- MobileQuiz/Views/Jogo.xaml.cs
+++ MobileQuiz/Views/Jogo.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileQuiz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace MobileQuiz.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Jogo : ContentPage
     {
-        public Jogo()
+        public Jogo(QuestionModel question)
         {
             InitializeComponent();
             InitButtons();

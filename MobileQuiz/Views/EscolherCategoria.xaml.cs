58f4be4155c9a81d8309a360b98d607362704b95
marcopandolfo <marcopandolfo577@gmail.com>
2019-09-20 22:31:47 -0300

    Handles de click das categorias

--- MobileQuiz/Views/EscolherCategoria.xaml.cs
+++ MobileQuiz/Views/EscolherCategoria.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MobileQuiz.Models;
+using MobileQuiz.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,28 @@ namespace MobileQuiz.Views
         public EscolherCategoria()
         {
             InitializeComponent();
+        }
+
+        private void Todas_Clicked(object sender, EventArgs e)
+        {
+            QuestionService.GetRandomQuestion();
+            Application.Current.MainPage = new Jogo();
+        }
+        private void Ciencia_Clicked(object sender, EventArgs e)
+        {
+            QuestionService.GetRandomQuestion("Ciencia");
+        }
+        private void Arte_Clicked(object sender, EventArgs e)
+        {
+            QuestionService.GetRandomQuestion("Arte");
+        }
+        private void Historia_Clicked(object sender, EventArgs e)
+        {
+            QuestionService.GetRandomQuestion("Historia");
+        }
+        private void Geografia_Clicked(object sender, EventArgs e)
+        {
+            QuestionService.GetRandomQuestion("Geografia");
         }
     }
 }

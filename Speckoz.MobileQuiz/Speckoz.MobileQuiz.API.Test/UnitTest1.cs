e22540b0366d7163031080b8e67bfe677d59ab88
Marco Antonio P <marcopandolfo577@gmail.com>
2019-12-21 02:48:44 -0300

    Criando projetos da api

---
+++ Speckoz.MobileQuiz/Speckoz.MobileQuiz.API.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Speckoz.MobileQuiz.API.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

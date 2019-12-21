fcc63db12cb2367d3aebd4bd5095af905e11d4ed
Marco Antonio P <marcopandolfo577@gmail.com>
2019-12-21 07:08:14 -0300

    Começando controller

---
+++ Speckoz.MobileQuiz/Speckoz.MobileQuiz.API/Controllers/QuestionsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Speckoz.MobileQuiz.API.Repository.Interfaces;
+
+namespace Speckoz.MobileQuiz.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        private IQuestionRepository _questionRepository;
+
+        public QuestionsController(IQuestionRepository questionRepository) =>
+            _questionRepository = questionRepository;
+        
+
+        // GET: api/Questions
+        [HttpGet]
+        public IActionResult GetRandomQuestion()
+        {
+            return Ok("Salve");
+        }
+    }
+}

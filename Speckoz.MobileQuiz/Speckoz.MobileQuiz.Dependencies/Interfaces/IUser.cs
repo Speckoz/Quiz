757a7eb7f1abc8fc38e6122fbf219b558f2d0b4d
Marco Antonio P <marcopandolfo577@gmail.com>
2019-12-23 16:13:30 -0300

    User model

---
+++ Speckoz.MobileQuiz/Speckoz.MobileQuiz.Dependencies/Interfaces/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speckoz.MobileQuiz.Dependencies.Interfaces
+{
+    public interface IUser
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public int Level { get; set; }
+        public string Password { get; set; }
+    }
+}

0faece8253d29c465ca3b8b5364230550a730e3e
Marco Antonio P <marcopandolfo577@gmail.com>
2020-01-03 18:46:33 -0300

    atualizando token do teste

--- Speckoz.Quiz/Quiz.API.Tests/ConnectionFactory.cs
+++ Speckoz.Quiz/Quiz.API.Tests/ConnectionFactory.cs
@@ -16,7 +16,7 @@ namespace Quiz.API.Tests
 
             // JWT
             client.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTY0MTAxNTY3NywiaXNzIjoiU3BlY2tveiIsImF1ZCI6IlNwZWNrb3oifQ.3X1RADhxOkPOkRIOTUB7KuqrcfTklLGsX-hfq4bjpp8");
+                new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImMyZTEyZjZmLWVjYTEtNGI0Ni04M2RhLTgwZDU3NDc1MzJkYyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNjQxMjQ2MjY2LCJpc3MiOiJTcGVja296IiwiYXVkIjoiU3BlY2tveiJ9.sW9tcYEBTo1R6o_XuVyPhOwjxK-F1mlMCeJw3uODjp8");
 
             return client;
         }

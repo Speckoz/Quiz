e01c1ee06b4229ffefff539881acb36e91a47d4e
Marco Antonio P <marcopandolfo577@gmail.com>
2020-01-04 16:48:18 -0300

    env

--- Speckoz.Quiz/Quiz.API/Startup.cs
+++ Speckoz.Quiz/Quiz.API/Startup.cs
@@ -69,15 +69,17 @@ namespace Quiz.API
         {
             if (env.IsDevelopment())
             {
-                // Auto create DB
-                using (IServiceScope serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-                {
-                    ApiContext context = serviceScope.ServiceProvider.GetRequiredService<ApiContext>();
-                    context.Database.Migrate();
-                }
                 app.UseDeveloperExceptionPage();
-                seedingService.Seed();
             }
+
+            // Auto create DB
+            using (IServiceScope serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                ApiContext context = serviceScope.ServiceProvider.GetRequiredService<ApiContext>();
+                context.Database.Migrate();
+            }
+
+            seedingService.Seed();
 
             app.UseAuthentication();
 

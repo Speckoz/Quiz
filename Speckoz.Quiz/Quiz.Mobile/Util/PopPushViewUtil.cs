9ba3b01b3686308079bd3827eeed7dae5e9ca1c9
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-01-09 18:48:10 -0500

    Aplicando novas cores dos cards

--- Speckoz.Quiz/Quiz.Mobile/Util/PopPushViewUtil.cs
+++ Speckoz.Quiz/Quiz.Mobile/Util/PopPushViewUtil.cs
@@ -8,13 +8,12 @@ namespace Quiz.Mobile.Util
     {
         public static async Task PushAsync(Page page, bool animated = false)
         {
-            await Task.Delay(200);
+            await Task.Delay(1000);
             await Application.Current.MainPage.Navigation.PushAsync(page, animated);
         }
 
         public static async Task PopAsync(bool animated = false)
         {
-            await Task.Delay(200);
             try
             {
                 await Application.Current.MainPage.Navigation.PopAsync(animated);
@@ -24,13 +23,12 @@ namespace Quiz.Mobile.Util
 
         public static async Task PushModalAsync(Page page, bool animated = false)
         {
-            await Task.Delay(200);
+            await Task.Delay(1000);
             await Application.Current.MainPage.Navigation.PushModalAsync(page, animated);
         }
 
         public static async Task PopModalAsync(bool animated = false)
         {
-            await Task.Delay(200);
             try
             {
                 await Application.Current.MainPage.Navigation.PopModalAsync(animated);
